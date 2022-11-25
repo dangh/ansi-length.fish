cb79e181746fc47ade4ac3da6058b19ef0075b32
Dang <huynhminhdang@gmail.com>
2022-11-25 09:52:13 +0700

    pure fish

--- functions/ansi-length.fish
+++ functions/ansi-length.fish
@@ -1,7 +1,12 @@
 function ansi-length -d 'print string length without ansi escape characters'
-  set -l text $argv
-  if not isatty stdin
-    read -z text
-  end
-  echo -n $text | perl -pe 's/\e\[[0-9;]*m(?:\e\[K)?//g' | string length
+  begin
+    if not isatty stdin
+      cat -
+    else
+      echo -n $argv
+    end
+  end \
+  | string replace -ra '\e\[[^m]*m' '' \
+  | string replace -ra '[^[:print:]]' '' \
+  | string length
 end
