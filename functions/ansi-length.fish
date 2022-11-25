82633f9a4bbfcdb65d51d1508e483f7afd1a477c
Dang <huynhminhdang@gmail.com>
2022-11-25 10:53:50 +0700

    return zero for no input, always success
    Update ansi-length.fish

--- functions/ansi-length.fish
+++ functions/ansi-length.fish
@@ -1,4 +1,5 @@
 function ansi-length -d 'print string length without ansi escape characters'
+  set -l empty 1
   begin
     if not isatty stdin
       cat -
@@ -8,5 +9,13 @@ function ansi-length -d 'print string length without ansi escape characters'
   end \
   | string replace -ra '\e\[[^m]*m' '' \
   | string replace -ra '[^[:print:]]' '' \
-  | string length
+  | string length \
+  | while read -l len
+    echo $len
+    set empty 0
+  end
+  if test $empty -eq 1
+    echo 0
+  end
+  true
 end
